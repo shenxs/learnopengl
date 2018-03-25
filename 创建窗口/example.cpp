df157f710086cf807d8a1638825eadebde511256
Richard <a247820400@gmail.com>
2018-03-25 21:30:27 +0800

    窗口上色，清除缓存

--- 创建窗口/example.cpp
+++ 创建窗口/example.cpp
@@ -4,10 +4,14 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window ,int width,int height);
+void error_callback(int error,const char* description);
+void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods);
 
 int main(){
 
   using namespace std;
+
+  glfwSetErrorCallback(error_callback);
 
   //初始化 glfw
   glfwInit();
@@ -31,27 +35,45 @@ int main(){
   //将窗口的上下文设定为当前线程的上下文
   glfwMakeContextCurrent(window);
 
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetKeyCallback(window, key_callback);
 
+  glewExperimental=GL_TRUE;
+  GLenum err=glewInit();
+  if(err!=GLEW_OK){
+    std::cout<<"glew 初始化失败,程序结束"<<std::endl;
+  }
 
   glViewport(0, 0, 800, 600);
-
-
   while(!glfwWindowShouldClose(window)){
-    glfwSwapBuffers(window);
 
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
     //检查事件触发并调用相应的函数
     glfwPollEvents();
   }
 
 
+  glfwDestroyWindow(window);
   glfwTerminate();
+
   return 0;
 }
 
 
 
+void framebuffer_size_callback(GLFWwindow* window ,int width,int height){
+  glViewport(0, 0, width, height);
+}
 
+void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods){
+  if(key == GLFW_KEY_ESCAPE && action ==GLFW_PRESS){
+    glfwSetWindowShouldClose(window, true);
+  }
+}
 
-
-
+void error_callback(int error,const char* description){
+  fputs(description, stderr);
+}
 
