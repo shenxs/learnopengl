77c1a8662c7e243852aa41df51c598c37c3c832f
Richard <a247820400@gmail.com>
2018-04-08 20:27:04 +0800

    修改头文件和makefile 方便ycmd补全函数的参数,否则都是macro

--- 创建三角形/example.cpp
+++ 创建三角形/example.cpp
@@ -1,5 +1,12 @@
-#include <GL/glew.h>
-#include <GL/glu.h>
+#ifdef USE_GLEW
+    #include <GL/glew.h>
+#else
+    #warning "Not using GLEW"
+    #define GL_GLEXT_PROTOTYPES
+    #include <GL/gl.h>
+    #include <GL/glext.h>
+#endif
+#include <GL/freeglut.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -50,36 +57,50 @@ int main(){
   //将窗口的上下文设定为当前线程的上下文
   glfwMakeContextCurrent(window);
 
+  //设置窗口大小变换的回调函数
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  //设置按键的回调函数
   glfwSetKeyCallback(window, key_callback);
 
+#ifdef USE_GLEW
+  //使用实验性质，必须加上,不然会报错
   glewExperimental=GL_TRUE;
   GLenum err=glewInit();
   if(err!=GLEW_OK){
     std::cout<<"glew 初始化失败,程序结束"<<std::endl;
   }
+#endif
 
   glViewport(0, 0, 800, 600);
 
 
+  //顶点们
   float vertices[] = {
-    -0.5f, -0.5f, 0.0f, //left
+   -0.5f, -0.5f, 0.0f,  //left
     0.5f, -0.5f, 0.0f,  //right
     0.0f,  0.5f, 0.0f   //top
   };
 
-
-  unsigned int  VBO,VAO;
+  unsigned int VBO, VAO;
+  //生成顶点数组
   glGenVertexArrays(1,&VAO);
+  //生成顶点缓冲
   glGenBuffers(1,&VBO);
 
+  //绑定当前操作的顶点数组对象
   glBindVertexArray(VAO);
+  //绑定当前的缓冲
   glBindBuffer(GL_ARRAY_BUFFER,VBO);
 
 
+  //将数据复制到buffer中
   glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 
+  //定义常规的顶点参数数组数据
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+
+
   glEnableVertexAttribArray(0);
 
   // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
@@ -89,9 +110,15 @@ int main(){
   // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
   glBindVertexArray(0);
 
+  //声明顶点着色器
   int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  //将着色器源码复制到 vertexShader 对象中
   glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
+  //编译顶点着色器
   glCompileShader(vertexShader);
+
+
+  //获取编译信息
   int success;
   char infoLog[512];
   glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
@@ -100,6 +127,7 @@ int main(){
     std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
 
+  //同理片段着色器
   int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
   glCompileShader(fragmentShader);
@@ -109,6 +137,7 @@ int main(){
     std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
 
+  //链接程序
   int shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
@@ -119,17 +148,21 @@ int main(){
     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
     std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
   }
+
+  //删除已经链接的着色器
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
+
   while(!glfwWindowShouldClose(window)){
 
+    //设定glClear使用的颜色
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
+
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
